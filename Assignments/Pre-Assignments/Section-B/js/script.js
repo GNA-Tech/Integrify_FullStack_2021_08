ee07318b55a94950e8067b6240a99a3409ebb489
ThriledLokki983 <gnimoh001@gmail.com>
2021-07-08 17:35:52 +0200

    added the maketask function and DOM manipulation actions

--- Assignments/Pre-Assignments/Section-B/js/script.js
+++ Assignments/Pre-Assignments/Section-B/js/script.js
@@ -6,10 +6,12 @@ const addButton = document.querySelector(".btn");
 const corrIcon = document.querySelector(".task__corr");
 const closeIcon = document.querySelector(".task__close");
 const taskContent = document.querySelector(".main__content");
+const parentElement = document.querySelector(".tasks");
 const taskBox = document.querySelector(".task");
 
 // hide the corr icon
 corrIcon.classList.add("hidden");
+const isClosed = true;
 
 /**
  * this function will take the task html element and all its associate properties and recreate
@@ -20,9 +22,11 @@ const makeTask = function (task) {
     //TODO
     // check if the task is not empty
 
+    // taskBox.innerHTML = "";
+
     let html = `
     <div class="task">
-    <div class="task__corr">
+    <div class="task__corr hidden">
         <svg class="corr">
             <use xlink:href="images/icons.svg#icon-check"></use>
         </svg>
@@ -40,7 +44,7 @@ const makeTask = function (task) {
 </div>
     `;
 
-    taskBox.insertAdjacentHTML("beforebegin", html);
+    taskBox.insertAdjacentHTML("afterend", html);
 };
 
 addButton.addEventListener("click", (e) => {
@@ -55,18 +59,21 @@ addButton.addEventListener("click", (e) => {
 
 const tasks = []; // in case I will use a different approach where I have to store all the tasks
 
-closeIcon.addEventListener("click", () => {
-    console.log("close button is now clicked"); // this is just for testing purposes
+/**
+ * Add corr icon before the task content.
+ * Change background to green.
+ * Apply a class - strikethrough to the task content.
+ * Revert back the color for the close button
+ */
+parentElement.addEventListener("click", (e) => {
+    const btn = e.target.closest(".task");
+    console.log(e.target); // for testing purposes
 
-    // 1. Add corr icon before the task content.
-    corrIcon.classList.remove("hidden");
-
-    // 2. Change background to green.
-    taskBox.classList.add("finish");
-
-    // 3. Apply a class - strikethrough to the task content.
-    taskContent.classList.add("done");
-
-    // 4. Revert back the color for the close button
-    closeIcon.classList.add("revert");
+    if (btn) {
+        corrIcon.classList.remove("hidden");
+        btn.classList.add("finish");
+        btn.classList.add("done");
+        btn.classList.add("revert");
+    }
+    if (!btn) return;
 });
