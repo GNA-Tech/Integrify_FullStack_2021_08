85e329ba1c76e235bebaeccdeea8252303e9d908
ThriledLokki983 <gnimoh001@gmail.com>
2021-07-10 00:28:42 +0200

    rewrote the entire add todo task js

--- Assignments/Pre-Assignments/Section-B/js/script.js
+++ Assignments/Pre-Assignments/Section-B/js/script.js
@@ -10,54 +10,75 @@ const parentElement = document.querySelector(".tasks");
 const taskBox = document.querySelector(".task");
 
 // hide the corr icon
-corrIcon.classList.add("hidden");
-const isClosed = true;
+// corrIcon.classList.add("hidden");
+const FINISH = "finish";
+const REVERT = "revert";
+const HIDDEN = "hidden";
+const UNHIDDEN = "unhidden";
+const DONE = "done";
+
+let tasks = []; // in case I will use a different approach where I have to store all the tasks
 
 /**
  * this function will take the task html element and all its associate properties and recreate
  * it if a new task is created
  * @param {*} task
  */
-const makeTask = function (task) {
-    //TODO
-    // check if the task is not empty
+const makeTask = function (task, isDone, close) {
+    if (close) return;
 
-    // taskBox.innerHTML = "";
+    const DONE = isDone ? UNHIDDEN : HIDDEN;
+    const TASK_CLOSE = isDone ? FINISH : "";
+    const CHANGE_COLOR = isDone ? REVERT : "";
 
     let html = `
-    <div class="task">
-    <div class="task__corr hidden">
-        <svg class="corr">
-            <use xlink:href="images/icons.svg#icon-check"></use>
-        </svg>
-    </div>
+    <li class="task ${TASK_CLOSE}">
+    <svg class="task__corr ${DONE} ${CHANGE_COLOR}">
+        <use
+            xlink:href="images/icons.svg#icon-check"
+            class="corr"
+            status="complete"
+        ></use>
+    </svg>
+
     <div class="task__content">
-        <p class="main__content">
+        <p class="main__content " status="task_item">
             ${task}
         </p>
     </div>
-    <div class="task__close">
-        <svg class="close">
-            <use xlink:href="images/icons.svg#icon-cross"></use>
-        </svg>
-    </div>
-</div>
+
+    <svg class="task__close ${CHANGE_COLOR}">
+        <use
+            xlink:href="images/icons.svg#icon-cross"
+            class="close"
+            status="delete"
+        ></use>
+    </svg>
+</li>
     `;
 
-    taskBox.insertAdjacentHTML("afterend", html);
+    taskBox.insertAdjacentHTML("beforebegin", html);
 };
 
 addButton.addEventListener("click", (e) => {
     e.preventDefault();
 
-    console.log("add button is clicked");
-
     const taskItem = formInput.value;
-    makeTask(taskItem);
+    if (taskItem) {
+        tasks.push(taskItem);
+        makeTask(taskItem, false);
+    } else {
+        alert("Task cannot be empty");
+    }
     formInput.value = "";
+    console.log(tasks);
 });
 
-const tasks = []; // in case I will use a different approach where I have to store all the tasks
+const finishTask = function (taskItem) {
+    corrIcon.classList.toggle(HIDDEN);
+    taskItem.classList.toggle(FINISH);
+    taskContent.classList.toggle(DONE);
+};
 
 /**
  * Add corr icon before the task content.
@@ -65,15 +86,11 @@ const tasks = []; // in case I will use a different approach where I have to sto
  * Apply a class - strikethrough to the task content.
  * Revert back the color for the close button
  */
-parentElement.addEventListener("click", (e) => {
-    const btn = e.target.closest(".task");
-    console.log(e.target); // for testing purposes
+closeIcon.addEventListener("click", function (e) {
+    const task = e.target.closest(".task");
+    console.log(task); // for testing purposes
 
-    if (btn) {
-        corrIcon.classList.remove("hidden");
-        btn.classList.add("finish");
-        btn.classList.add("done");
-        btn.classList.add("revert");
+    if (task) {
+        finishTask(task);
     }
-    if (!btn) return;
 });
